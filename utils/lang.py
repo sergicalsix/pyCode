e6aed67a40bc25332bc84e7d75c0843ce7b0b0df
sergicalsix <kenp3434membertip1@gmail.com>
2021-11-29 00:30:37 +0900

    add: utils/ kana

--- utils/lang.py
+++ utils/lang.py
@@ -1,8 +1,11 @@
 import re
 
+# 郵便番号検出で用いた
+# https://note.nkmk.me/python-re-regex-character-type/
 def kana(str_)->bool:
-    p = re.search('+')
-    if p == None:
+    p = re.compile('[\u30A1-\u30FF]+')
+    result = p.match(str_)
+    if result  == None:
         return False
     return True
     
